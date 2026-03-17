763a05520425aa6f198543e94b15cda116993b34
Akm1-55 <akmal.emad05@eng-st.cu.edu.eg>
2026-03-17 23:41:21 +0200

    fix(auth): fixed the commit before by making SignInForm not get props

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -38,7 +38,7 @@ export default function Page() {
         <div className="flex flex-center flex-col items-center h-fit justify-center bg-bg-base border border-text-on-brand px-1.5 py-5">
           <ReCaptchaProvider>
             {/* {isMounted && <SignInForm />} */}
-            <SignInForm></SignInForm>
+            <SignInForm />
           </ReCaptchaProvider>
 
         </div>
