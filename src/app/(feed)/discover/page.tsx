ceff71717205025f0331a08d87f8b3269dad4c0c
Akm1-55 <akmal.emad05@eng-st.cu.edu.eg>
2026-03-19 03:18:26 +0200

    feat(style): use playlist card in discover, uploaded default image to test

--- src/app/(feed)/discover/page.tsx
+++ src/app/(feed)/discover/page.tsx
@@ -1,3 +1,16 @@
+import PlaylistCard from "@/components/PlaylistCard";
+
 export default function Page() {
-  return <div></div>;
+  return(
+        <div className="flex gap-4 flex-wrap">
+      <PlaylistCard
+        title="Maara - Who Let The.."
+        coverUrl="/images/default_song_image.png"
+      />
+      <PlaylistCard
+        title="Another Playlist"
+        coverUrl="/images/default_song_image.png"
+      />
+    </div>
+  );
 }
