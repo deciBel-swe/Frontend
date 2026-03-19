b4b31298b6803de32f281ae0459c444e0a2ba0b7
Omar Ahmed <152551143+omar27105@users.noreply.github.com>
2026-03-19 23:07:52 +0200

    Reapply "refactor (prof)"
    This reverts commit c7216a0d8e64b6945899cc5bd85af290af560836.

--- src/app/[username]/layout.tsx
+++ src/app/[username]/layout.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ProfileHeader from '@/features/prof/components/ProfileHeader';
-import ProfileNav from '@/features/prof/components/ProfileNav';
+import ProfileSideBar from '@/features/prof/components/ProfileSideBar';
 import { IconButton } from '@/components/buttons/IconButton';
 import { ShareIcon, EditIcon } from '@/components/icons/GenrealIcons';
-import ProfileSideBar from '@/features/prof/components/ProfileSideBar';
-import NavNControlBar from '@/features/prof/components/NavNControlBar';
+import ProfileNav from '@/features/prof/components/ProfileNav';
 
 const Layout = async ({
   children,
@@ -20,7 +19,20 @@ const Layout = async ({
       <div>
         <ProfileHeader username={`${username}`} />
       </div>
-      <NavNControlBar username={`${username}`} />
+      <div className="flex flex-row-reverse items-center gap-18 mb-5 mr-10">
+        <IconButton aria-label="edit">
+          <span className="flex items-center bg-gray-300 rounded-md px-4 py-2 flex-shrink-0 dark:bg-gray-700">
+            <EditIcon /> edit
+          </span>
+        </IconButton>
+
+        <IconButton aria-label="Share">
+          <span className="flex items-center bg-gray-300 rounded-md px-4 py-2 flex-shrink-0 dark:bg-gray-700">
+            <ShareIcon /> share
+          </span>
+        </IconButton>
+        <ProfileNav username={`${username}`} />
+      </div>
       <div className="flex w-full mt-6 px-8">
         <div className="flex-1">{children}</div>
         <div className="w-[340px] ml-10">
