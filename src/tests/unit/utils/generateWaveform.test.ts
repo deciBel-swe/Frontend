7854a942be4396759868f8ce434a048c334e6041
Ahmed Salah <ahmed10salah111@gmail.com>
2026-03-20 20:00:35 +0200

    hotfix: fix type error in waveform test

--- src/tests/unit/utils/generateWaveform.test.ts
+++ src/tests/unit/utils/generateWaveform.test.ts
@@ -6,7 +6,7 @@ const mockAudioContext = (channelData: Float32Array) => {
   const getChannelData = jest.fn().mockReturnValue(channelData);
   const decodeAudioData = jest
     .fn()
-    .mockResolvedValue({ getChannelData } as AudioBuffer);
+    .mockResolvedValue({ getChannelData } as unknown as AudioBuffer);
 
   const AudioContextMock = jest.fn().mockImplementation(() => ({
     decodeAudioData,
