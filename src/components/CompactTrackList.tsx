b99e51123973ae1288e8b39b99c0eaa8f2448101
Akm1-55 <akmal.emad05@eng-st.cu.edu.eg>
2026-03-26 19:39:30 +0200

    fix(style): almost modified the scrollbar

--- src/components/CompactTrackList.tsx
+++ src/components/CompactTrackList.tsx
@@ -25,7 +25,12 @@ export default function CompactTrackList({ tracks }: CompactTrackListProps) {
   return (
     <div className="sound__trackList">
       {/* WRAPPER WITH MAX HEIGHT */}
-      <div className="max-h-[calc(10*2.5rem)] overflow-y-auto">
+      <div className="max-h-[calc(10*2.5rem)] overflow-y-auto 
+    [&::-webkit-scrollbar]:w-1 
+    [&::-webkit-scrollbar-thumb]:bg-gray-400/20
+    [&::-webkit-scrollbar-thumb]:rounded-full
+    hover:[&::-webkit-scrollbar-thumb]:bg-gray-400/60
+      ">
         <ul className="trackList__item sc-list-nostyle">
           {visibleTracks.map((track, index) => (
             <CompactTrackItem key={track.id} track={track} index={index} />
