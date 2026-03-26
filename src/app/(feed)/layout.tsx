6ed0f49439b94773c49b48187558645f9cf50d6e
Akm1-55 <akmal.emad05@eng-st.cu.edu.eg>
2026-03-26 16:25:14 +0200

    fix(style): made the page start from the same horizontal line

--- src/app/(feed)/layout.tsx
+++ src/app/(feed)/layout.tsx
@@ -61,22 +61,24 @@ type LayoutProps = {
 export default function Layout({ children }: LayoutProps) {
   // return <>{children}</>;
   return (
-   <div className="w-full">
-  <div className="flex w-full gap-6">
-    {/* MAIN */}
-  <div className="flex-1 min-w-0 flex flex-col pt-6">
-    {children}
-  </div>
-  {/* SIDEBAR */}
-  <aside className="hidden lg:block w-[340px] flex-shrink-0 flex flex-col pt-6">
-    <Sidebar
-      History_header="Listening history"
-      Artist_header="Artists you should follow"
-      artists={artists}
-      history={history}
-    />
-    </aside>
-  </div>
+    <div className="w-full">
+      <div className="flex w-full gap-6 items-start mt-6">
+        {/* MAIN CONTENT */}
+        <main className="flex-1 min-w-0">
+          {children}
+        </main>
+
+        {/* SIDEBAR */}
+        <aside className="hidden lg:block w-[340px] flex-shrink-0 min-w-0 mt-3">
+          <Sidebar
+            History_header="Listening history"
+            Artist_header="Artists you should follow"
+            artists={artists}
+            history={history}
+          />
+        </aside>
+      </div>
     </div>
+
   );
 }
