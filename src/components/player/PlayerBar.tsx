eb6e97d0d383e4efdd274f9dc50c0aa64596c576
Akm1-55 <akmal.emad05@eng-st.cu.edu.eg>
2026-04-03 16:48:19 +0200

    feat(style): add a playerbar component

---
+++ src/components/player/PlayerBar.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import PlayerControls from "./PlayerControls";
+import Timeline from "./Timeline";
+import VolumeControl from "./VolumeControl";
+import SoundBadge from "./SoundBadge";
+import QueuePanel from "./QueuePanel";
+import PlaybackModes from "./PlaybackModes";
+import SocialActions from "./SocialActions";
+
+interface QueueInfo {
+  visible: boolean;
+  items: { title: string; artist?: string }[];
+}
+
+interface PlayerBarProps {
+  track: string;
+  artist: string;
+  artwork: string;
+  duration: number;
+  currentTime: number;
+  isPlaying: boolean;
+  onPlay: () => void;
+  onPause: () => void;
+  onNext: () => void;
+  onPrev: () => void;
+  onScrub: (time: number) => void;
+  volume: number;
+  onVolumeChange: (v: number) => void;
+  queue: QueueInfo;
+  onQueueToggle: () => void;
+}
+
+export default function PlayerBar(props: PlayerBarProps) {
+  const {
+    track,
+    artist,
+    artwork,
+    duration,
+    currentTime,
+    isPlaying,
+    onPlay,
+    onPause,
+    onNext,
+    onPrev,
+    onScrub,
+    volume,
+    onVolumeChange,
+    queue,
+    onQueueToggle,
+  } = props;
+
+  return (
+    /* The main bar background - stays 100% width */
+    <div className="sticky bottom-0 w-full bg-[#303030] border-t border-[#333] z-50">
+      
+      {/* The Content Div:
+          - max-w-[1240px]: This makes the "content area" smaller.
+          - mx-auto: Centers that smaller area.
+          - flex items-center: Keeps everything vertically centered.
+      */}
+      <div className="max-w-[1240px] mx-auto flex items-center h-[52px] px-2">
+        
+        <PlayerControls
+          isPlaying={isPlaying}
+          onPlay={onPlay}
+          onPause={onPause}
+          onNext={onNext}
+          onPrev={onPrev}
+        />
+
+        <PlaybackModes />
+        
+        <div className="flex-1">
+          <Timeline
+            currentTime={currentTime}
+            duration={duration}
+            onScrub={onScrub}
+          />
+        </div>
+
+        <VolumeControl value={volume} onChange={onVolumeChange} />
+
+        <SoundBadge track={track} artist={artist} artwork={artwork} />
+        
+        <SocialActions />
+      </div>
+
+      <QueuePanel queue={queue} onClose={onQueueToggle} />
+    </div>
+  );
+}
