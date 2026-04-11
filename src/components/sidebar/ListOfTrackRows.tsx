85a6cd1550c6b20b269a55075da070e7699cc02a
Akm1-55 <akmal.emad05@eng-st.cu.edu.eg>
2026-04-11 21:35:38 +0200

    fix/style: adjusted hovering

--- src/components/sidebar/ListOfTrackRows.tsx
+++ src/components/sidebar/ListOfTrackRows.tsx
@@ -31,19 +31,19 @@ export default function ARTIST ({ history, History_header, headerUrl, queueSourc
             /* ================= ARTISTS ================= */
       <section>
         <div className="flex justify-between items-center border-b border-border-default pb-2">
-          <Link href={headerUrl} className="text-sm font-semibold text-text-muted">
+          <Link href={headerUrl} className="text-[11px] font-bold uppercase text-text-secondary">
             {History_header}
           </Link>
 
           <Link
             href={headerUrl}
-            className="text-xs text-text-muted hover:text-text-primary transition"
+            className="text-text-secondary hover:text-text-primary text-[11px]"
           >
             View all
           </Link>
         </div>
 
-        <div className="flex flex-col gap-3 mt-4 max-h-105 overflow-y-auto pr-1">
+        <div className="flex flex-col gap-3 max-h-105 overflow-y-auto pr-1">
           {history.map((track, i) => (
             <TrackRow
               key={`${track.trackId ?? track.title}-${i}`}
