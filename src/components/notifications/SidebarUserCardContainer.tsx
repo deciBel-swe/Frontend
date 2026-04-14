6f0b7f05b3adc5a5f5d5ddb456d91cc81728fbf7
Akm1-55 <akmal.emad05@eng-st.cu.edu.eg>
2026-04-14 20:01:23 +0200

    fix: commented my custom follow btn

--- src/components/notifications/SidebarUserCardContainer.tsx
+++ src/components/notifications/SidebarUserCardContainer.tsx
@@ -6,18 +6,18 @@ import { SidebarUserCard } from "./SidebarUserCard";
 type Props = React.ComponentProps<typeof SidebarUserCard>;
 
 export function SidebarUserCardContainer(props: Props) {
-  const [isFollowing, setIsFollowing] = useState(props.isFollowing ?? false);
+  // const [isFollowing, setIsFollowing] = useState(props.isFollowing ?? false);
 
-  const handleFollowToggle = () => {
-    setIsFollowing((prev) => !prev);
-    console.log("toggle follow");
-  };
+  // const handleFollowToggle = () => {
+  //   setIsFollowing((prev) => !prev);
+  //   console.log("toggle follow");
+  // };
 
   return (
     <SidebarUserCard
       {...props}
-      isFollowing={isFollowing}
-      onFollowToggle={handleFollowToggle}
+      // isFollowing={isFollowing}
+      // onFollowToggle={handleFollowToggle}
     />
   );
 }
